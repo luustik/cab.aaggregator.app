9ddb4bc91dca4a668c76649761316fc4ba4139e7
lustikkkkk <kirill.lustoff@gmail.com>
2024-10-07 16:50:28 +0300

    added validation and signature for swagger

--- passenger-service/src/main/java/cab/aggregator/app/passengerservice/controller/PassengerControllerImpl.java
+++ passenger-service/src/main/java/cab/aggregator/app/passengerservice/controller/PassengerControllerImpl.java
@@ -6,7 +6,10 @@ import cab.aggregator.app.passengerservice.dto.response.PassengerResponse;
 import cab.aggregator.app.passengerservice.dto.validation.OnCreate;
 import cab.aggregator.app.passengerservice.dto.validation.OnUpdate;
 import cab.aggregator.app.passengerservice.service.PassengerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,44 +20,58 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/passengers")
 @RequiredArgsConstructor
 @Validated
+@Tag(name="Passenger controller")
 public class PassengerControllerImpl {
 
     private final PassengerService passengerService;
 
+    @Operation(summary = "Get passenger by Id")
     @GetMapping("/{id}")
     public PassengerResponse getPassengerById(@PathVariable int id) {
         return passengerService.getPassengerById(id);
     }
 
+    @Operation(summary = "Get all passengers")
     @GetMapping
     public PassengerContainerResponse getAllPassengers(){
         return passengerService.getAllPassengers();
     }
 
+    @Operation(summary = "Get passenger by phone")
     @GetMapping("/passenger-by-phone/{phone}")
-    public PassengerResponse getPassengerByPhone(@Valid @Validated
-                                                         @PathVariable String phone) {
+    public PassengerResponse getPassengerByPhone(@Valid
+                                                 @Validated
+                                                 @PathVariable
+                                                 @Pattern(regexp = "\\+375\\((29|44|33|25)\\)\\d{7}$",
+                                                          message = "{passengerPhone.pattern}") String phone) {
         return passengerService.getPassengerByPhone(phone);
     }
 
+    @Operation(summary = "Get passenger by email")
     @GetMapping("/passenger-by-email/{email}")
-    public PassengerResponse getPassengerByEmail(@Valid @Validated
-                                                 @PathVariable String email) {
+    public PassengerResponse getPassengerByEmail(@Valid
+                                                 @Validated
+                                                 @PathVariable
+                                                 @Pattern(regexp = "^[\\w.-]+@[\\w.-]+\\.[\\w.-]+$",
+                                                          message = "{passengerEmail.pattern}") String email) {
         return passengerService.getPassengerByEmail(email);
     }
 
+    @Operation(summary = "Soft delete passenger")
     @DeleteMapping("/soft/{id}")
     public ResponseEntity<PassengerResponse> softDeleteDriverById(@PathVariable int id) {
         passengerService.softDeletePassenger(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Hard delete passenger")
     @DeleteMapping("/{id}")
     public ResponseEntity<PassengerResponse> hardDeleteDriverById(@PathVariable int id) {
         passengerService.hardDeletePassenger(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Create new passenger")
     @PostMapping
     public ResponseEntity<PassengerResponse> createDriver(@Valid @Validated(OnCreate.class)
                                                        @RequestBody PassengerRequest request) {
@@ -62,6 +79,7 @@ public class PassengerControllerImpl {
                 .body(passengerService.createPassenger(request));
     }
 
+    @Operation(summary = "Update passenger by Id")
     @PutMapping("/{id}")
     public PassengerResponse updateDriver(@PathVariable int id,
                                        @Valid @Validated(OnUpdate.class) @RequestBody PassengerRequest request) {
