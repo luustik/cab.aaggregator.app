143489fe8882131cc28cf45be92cf6fe11033114
lustikkkkk <kirill.lustoff@gmail.com>
2024-10-10 00:07:37 +0300

    fixed bug in Constants and Utilities

--- ride-service/src/main/java/cab/aggregator/app/rideservice/utility/Constants.java
+++ ride-service/src/main/java/cab/aggregator/app/rideservice/utility/Constants.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class Constants {
+public final class Constants {
     public static final String REGEXP_STATUS = "^(?i)(CREATED|ACCEPTED|WAY_TO_PASSENGER|WAY_TO_DESTINATION|COMPLETED|CANCELLED)$";
     public static final String REGEXP_DATE_TIME = "^(20[0-9]{2}|21[0-0])-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$";
 }
--- ride-service/src/main/java/cab/aggregator/app/rideservice/utility/Utilities.java
+++ ride-service/src/main/java/cab/aggregator/app/rideservice/utility/Utilities.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class Utilities {
+public final class Utilities {
 
     public static LocalDateTime convertStringToLocalDateTime(String localDateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
