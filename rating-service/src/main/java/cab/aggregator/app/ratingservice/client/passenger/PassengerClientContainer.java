14d012c18d46f50df667011bfae89565e9922a86
lustikkkkk <kirill.lustoff@gmail.com>
2024-10-26 12:01:56 +0300

    implementation pattern circuit breaker

--- rating-service/src/main/java/cab/aggregator/app/ratingservice/client/passenger/PassengerClientContainer.java
+++ rating-service/src/main/java/cab/aggregator/app/ratingservice/client/passenger/PassengerClientContainer.java
@@ -1,16 +1,25 @@
 package cab.aggregator.app.ratingservice.client.passenger;
 
 import cab.aggregator.app.ratingservice.dto.client.PassengerResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PassengerClientContainer {
 
     private final PassengerClient passengerClient;
 
+    @CircuitBreaker(name = "circuit-breaker", fallbackMethod = "fallback")
     public PassengerResponse getById(int id) {
         return passengerClient.getPassengerById(id);
+    }
+
+    private void fallback(Exception ex) throws Exception {
+        log.warn(ex.getMessage(), ex);
+        throw  ex;
     }
 }
--- ride-service/src/main/java/cab/aggregator/app/rideservice/client/driver/DriverClientContainer.java
+++ ride-service/src/main/java/cab/aggregator/app/rideservice/client/driver/DriverClientContainer.java
@@ -1,16 +1,25 @@
 package cab.aggregator.app.rideservice.client.driver;
 
 import cab.aggregator.app.rideservice.dto.client.DriverResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DriverClientContainer {
 
     private final DriverClient driverClient;
 
+    @CircuitBreaker(name = "circuit-breaker", fallbackMethod = "fallback")
     public DriverResponse getById(int id) {
         return driverClient.getDriverById(id);
+    }
+
+    private void fallback(Exception ex) throws Exception {
+        log.warn(ex.getMessage(), ex);
+        throw  ex;
     }
 }
