f7d43bebb092662c2b8ee9b58a44bcffccd7a5b7
lustikkkkk <kirill.lustoff@gmail.com>
2024-10-27 19:22:47 +0300

    add config server

---
+++ config-server/src/test/java/cab/aggregator/app/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cab.aggregator.app.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
